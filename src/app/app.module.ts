649b7e68b4a4a75f039674cb5984d8c0ad5b2d02
Omar <3omar.chaari@gmail.com>
2021-08-22 15:34:40 +0100

    installation Restangular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RestangularModule, Restangular } from 'ngx-restangular';
+import { ProductsComponent } from './components/products/products.component';
+
+export function RestangularConfigFactory (RestangularProvider) {
+  RestangularProvider.setBaseUrl('https://127.0.0.1:8000/ws');
+  RestangularProvider.setDefaultHeaders({'Authorization': 'Bearer UDXPx-Xko0w4BRKajozCVy20X11MRZs1'});
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RestangularModule.forRoot(RestangularConfigFactory),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
