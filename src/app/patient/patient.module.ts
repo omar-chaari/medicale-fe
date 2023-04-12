0f14fd2bb2b0e75acadc6967e6bfc5f7ab0511b4
Omar <3omar.chaari@gmail.com>
2023-04-12 22:52:26 +0100

    update

--- src/app/patient/patient.module.ts
+++ src/app/patient/patient.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PatientRoutingModule } from './patient-routing.module';
+import { RendezVousComponent } from './rendez-vous/rendez-vous.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RendezVousComponent
+  ],
   imports: [
     CommonModule,
     PatientRoutingModule,
